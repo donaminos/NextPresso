4e3e057817ee560f1beea80b9fe622b513dff594
Amine Sadry <amine.sadry@gmail.com>
2023-06-26 08:23:51 +0200

    chore: init the codebase as Next project 🎉

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div className="mx-auto w-1/2 text-center">
+      <h1 className="text-3xl font-bold">
+        NextPresso<span className="text-sm font-semibold">.js</span>
+      </h1>
+      <p className="text-lg text-black">The Next blog for indie hackers</p>
+    </div>
+  );
+}
